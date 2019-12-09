35f7cf85d97dad4a10211d3069bea385e36a9d3e
fatima <benyamna2011@outlook.de>
2019-12-09 12:15:52 +0100

    +++

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Work on your tasks here
+
+
+
+    var submit = $('.submit')
+    var modal=$('.modal')
+    submit.on('click',event=>{
+modal.show()
+    })
+    modal.on('click',event =>{
+        console.log("modal click")
+        modal.hide()
+    })
+
+      
